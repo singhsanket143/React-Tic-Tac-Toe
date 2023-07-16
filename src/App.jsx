74ea707cdbd1f974502a3e22b514d3ec961bdf70
Sanket Singh <singhsanket143@gmail.com>
2023-07-16 20:22:14 +0530

    Added tic tac toe game

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Grid from './components/Grid/Grid';
+
+function App() {
+  return (
+    <>
+      <Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import Icon from "../Icon/Icon";
+
+import './Card.css';
+
+function Card({ onPlay, player, index, gameEnd }) {
+
+    let icon = <Icon />
+    if(player == "X") {
+        icon = <Icon name={"cross"} />
+    } else if(player == "O") {
+        icon = <Icon name={"circle"} />
+    } 
+    return (
+        <div className="card" onClick={() => !gameEnd && player=="" && onPlay(index)}>  
+            {icon}
+        </div>
+    )
+}
+
+export default Card;
